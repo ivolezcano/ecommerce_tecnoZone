8c570c6f8416dfb29ca6bba78e0993fc30c24350
Ivan Lezcano <lezcamati@gmail.com>
2024-02-24 16:23:28 -0300

    add API 100%

--- tecnozone/app/product/[id]/page.jsx
+++ tecnozone/app/product/[id]/page.jsx
@@ -1,18 +1,18 @@
 
 async function getData (id) {
   const response = await fetch(`https://fake-store-api-409620.rj.r.appspot.com/api/products/${id}`)
-  const content = await response.json()
-  return content.content
+  const data = await response.json()
+  return data
 }
 
 async function shopPage({ params }) {
 
-  const product = await getData(params.id)
+  const indProduct = await getData(params.id)
 
   return (
     <div>
         <h1>Product Page</h1>
-        <p>Name: {product.id}</p>
+        <p>{indProduct.name}</p>
     </div>
   )
 }
--- tecnozone/components/Products.jsx
+++ tecnozone/components/Products.jsx
@@ -1,11 +1,21 @@
 'use client';
 import React from 'react'
+import { useRouter } from 'next/navigation';
 
 function Products({ products }) {
+
+  const router = useRouter();
+
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.name}</li>
+        <li key={product.id}
+        style={{cursor:'pointer'}}
+        className='list-group-item d-flex justify-content-between align-items-center list-group-item-action'
+          onClick={() => {
+            router.push(`/product/${product.id}`)
+          }}
+        >{product.name} ${product.price}</li>
       ))}
     </ul>
   )
